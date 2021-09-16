b22f1284fa528b7ef2e8ef9ed76cd224dd43b549
Raeymakers <sofie.raeymakers@outlook.com>
2021-09-16 10:07:44 +0200

    Update Shiny exercise2.R

--- Shiny exercise2.R
+++ Shiny exercise2.R
@@ -1,9 +1,12 @@
 library(shiny)
 
-ui<- fluidPage()
+ui<- fluidPage(
+  sliderInput(inputId= "num", label="choose a number", value=25, min=1, max=100),
+  plotOutput(outputId="hist")
+)
 
 server <- function (input, output){
-  
+  output$hist<- renderPlot({hist(rnorm(100))})
 }
 
 shinyApp(ui=ui, server=server)
