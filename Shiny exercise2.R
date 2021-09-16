bf147cafe639174aa4f09174e07888f59527e1a3
Raeymakers <sofie.raeymakers@outlook.com>
2021-09-16 10:26:26 +0200

    Update Shiny exercise2.R

--- Shiny exercise2.R
+++ Shiny exercise2.R
@@ -1,12 +1,24 @@
 library(shiny)
 
-ui<- fluidPage(
+ui<- fluidPage( # makes the User Interface
   sliderInput(inputId= "num", label="choose a number", value=25, min=1, max=100),
   plotOutput(outputId="hist")
 )
 
-server <- function (input, output){
-  output$hist<- renderPlot({hist(rnorm(100))})
+server <- function (input, output){ #we have to add this to the server
+  output$hist<- renderPlot({ # here we say what we want to do with output$hist and make it with render
+    title <- "100 random normal values" #you can add as many code as you want between these braces
+    hist(rnorm(input$num))
+    })
 }
 
+#use server function to assemple inputs into outputs. 3 rules:
+# -save output that you build to output$
+# - build the output with a render*() function()
+# -access input values with input$
 shinyApp(ui=ui, server=server)
+
+##you MUST use the name app.R if you want to share it on the web. 
+#shinyapps.io free website that allows you to run your app on a distant server, then pair with shinyapps account
+
+#go to https://vimeo.com/rstudioinc/review/131218530/212d8a5a7a/ at 30:00 to see how to launch the app on a server
