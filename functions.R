0d95dcea104c39593f8bd5444797898cb5b1c0a9
Raeymakers <sofie.raeymakers@outlook.com>
2022-02-25 10:58:12 +0100

    functions code cleaning
    deleting everything that doesn't get used

--- functions.R
+++ functions.R
@@ -1,4 +1,5 @@
-# violin function
+### violin functions
+
 geom_flat_violin <-
   function(mapping = NULL,
            data = NULL,
@@ -64,90 +65,7 @@ GeomFlatViolin <- ggproto(
   ),
   required_aes = c("x", "y")
 )
-#standard error function
-standard_error <- function(x)
-  sd(x) / sqrt(length(x))
 
-#function for split violin
-GeomSplitViolin <- ggproto(
-  "GeomSplitViolin",
-  GeomViolin,
-  draw_group = function(self, data, ..., draw_quantiles = NULL) {
-    data <-
-      transform(
-        data,
-        xminv = x - violinwidth * (x - xmin),
-        xmaxv = x + violinwidth * (xmax - x)
-      )
-    grp <- data[1, "group"]
-    newdata <-
-      plyr::arrange(transform(data, x = if (grp %% 2 == 1)
-        xminv
-        else
-          xmaxv), if (grp %% 2 == 1)
-            y
-        else
-          - y)
-    newdata <-
-      rbind(newdata[1, ], newdata, newdata[nrow(newdata), ], newdata[1, ])
-    newdata[c(1, nrow(newdata) - 1, nrow(newdata)), "x"] <-
-      round(newdata[1, "x"])
-    if (length(draw_quantiles) > 0 &
-        !scales::zero_range(range(data$y))) {
-      stopifnot(all(draw_quantiles >= 0), all(draw_quantiles <=
-                                                1))
-      quantiles <-
-        ggplot2:::create_quantile_segment_frame(data, draw_quantiles)
-      aesthetics <-
-        data[rep(1, nrow(quantiles)), setdiff(names(data), c("x", "y")), drop = FALSE]
-      aesthetics$alpha <-
-        rep(1, nrow(quantiles))
-      both <- cbind(quantiles, aesthetics)
-      quantile_grob <-
-        GeomPath$draw_panel(both, ...)
-      ggplot2:::ggname("geom_split_violin",
-                       grid::grobTree(GeomPolygon$draw_panel(newdata, ...), quantile_grob))
-    }
-    else {
-      ggplot2:::ggname("geom_split_violin",
-                       GeomPolygon$draw_panel(newdata, ...))
-    }
-  }
-)
-
-geom_split_violin <-
-  function(mapping = NULL,
-           data = NULL,
-           stat = "ydensity",
-           position = "identity",
-           ...,
-           draw_quantiles = NULL,
-           trim = TRUE,
-           scale = "area",
-           na.rm = FALSE,
-           show.legend = NA,
-           inherit.aes = TRUE) {
-    layer(
-      data = data,
-      mapping = mapping,
-      stat = stat,
-      geom = GeomSplitViolin,
-      position = position,
-      show.legend = show.legend,
-      inherit.aes = inherit.aes,
-      params = list(
-        trim = trim,
-        scale = scale,
-        draw_quantiles = draw_quantiles,
-        na.rm = na.rm,
-        ...
-      )
-    )
-  }
-
-rescalepostive <- function(x) {
-  (x - min(x)) / (max(x) - min(x)) + 0.000000001
-}
 
 # Trait viz
 traitplot <-function(data, emmean_dataframe, var, title){
@@ -200,7 +118,6 @@ stateplot <-function(data, emmean_dataframe, var, title){
 }
 
 # overall correlation plot
-
 overall_corr <- function(PSS, PTQ, x_lab, y_lab) {
   dataframe <- data.frame(PSS, PTQ)
   ggscatter(dataframe, x = "PSS", y = "PTQ",
@@ -212,7 +129,6 @@ overall_corr <- function(PSS, PTQ, x_lab, y_lab) {
 }
 
 #cohen's d for traits
-
 cohens_d_trait <- function (var){
   noPMS_mu <-mean(data$var[data$PMS=='noPMS'], na.rm=TRUE)
   PMS_mu <-mean(data$var[data$PMS=='PMS'], na.rm=TRUE)
